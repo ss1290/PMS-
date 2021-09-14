ae693aa7b8f9ae3e24d821e1addc3f2ae40826b9
hrdy <sonushashankmot@gmail.com>
2021-09-14 11:54:22 +0530

    first release

--- public/js/staffprofile.js
+++ public/js/staffprofile.js
@@ -7,12 +7,13 @@ const editPhoneNo=document.querySelector('#editstaffPhoneNo')
 const editNewPassword=document.querySelector('#editstaffNewPassword')
 const editConfirmPassword=document.querySelector('#editstaffConfirmPassword')
 var {token}=Qs.parse(location.search,{ignoreQueryPrefix:true})
+var message=document.querySelector('#message')
 staffprofile.addEventListener('submit',(e)=>{
     e.preventDefault()
     fetch(`/profile?token=${token}&name=${editName.value}&email=${editEmail.value}&oldpassword=${editPassword.value}&address=${editAddress.value}&phoneNumber=${editPhoneNo.value}&newpassword=${editNewPassword.value}&confirmpassword=${editConfirmPassword.value}`).then((response)=>{
         response.json().then((data)=>{
             if(data.Error){
-                alert(data.Error)
+                message.textContent=data.Error
             }else if(data.user){
                 message.textContent=''
                 alert('Profile Updated')
